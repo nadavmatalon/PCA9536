acd0e856fb66c5a3a4f5698bc8e20562d3a1709e
Nadav <nadav.matalon@gmail.com>
2016-10-26 19:42:36 +0100

    added code & example for device info string friend function

---
+++ examples/PCA9536_Info/PCA9536_Info.ino
@@ -0,0 +1,106 @@
+/* 
+  PCA9536 LIBRARY - DEVICE INFORMATION STRING EXAMPLE
+  ---------------------------------------------------
+
+  INTRODUCTION
+  ------------
+  This sketch presents a minimal example of extending the PCA9536 Library to include an additional function for generating a printable device
+  information string which may be useful, for example, during debugging sessions.
+
+  As can be seen in the sketch below, implementation of this extended functionality only requires adding a single 'include' to the code, namely that of the relevant *.h file (PCA9536InfoStr.h).
+  
+  Note that this functional extension does come at the cost of an increased memory usage, and therefore it seemed preferable to maintain it 
+  as an optional add-on rather than include it in the core PCA9536 Library itself.
+
+  WIRING DIAGRAM
+  --------------
+
+               PCA9536
+               -------
+        IO0 --| •     |-- VCC
+              |       |
+        IO1 --|       |-- SDA
+              |       |
+        IO2 --|       |-- SCL
+              |       |
+        GND --|       |-- IO3
+               -------
+
+  This hookup can be used with the following sketch:
+
+  PIN 1 (IO0) - Leave unconnected for the purpose of this sketch
+  PIN 2 (IO1) - Leave unconnected for the purpose of this sketch
+  PIN 3 (IO2) - Leave unconnected for the purpose of this sketch
+  PIN 4 (GND) - Connect to Arduino GND
+  PIN 5 (IO3) - Leave unconnected for the purpose of this sketch
+  PIN 6 (SCL) - Connect to Arduino Pin A5 with a 2K2 (400MHz) or 10K (100MHz) pull-up resistor
+  PIN 7 (SDA) - Connect to Arduino Pin A4 with a 2K2 (400MHz) or 10K (100MHz) pull-up resistor
+  PIN 8 (VCC) - Connect to Arduino 5V output
+  DECOUPING   - Connect a 0.1uF Ceramic Capacitor between the PCA9536's VCC & GND pins
+
+  I2C ADDRESS
+  -----------
+  The PCA9536 has a single I2C address (factory hardwired):
+
+    PART                DEVICE I2C ADDRESS         PART
+   NUMBER           (BIN)     (HEX)     (DEC)     MARKING
+  --------        --------    -----     -----     -------
+  PCA9536D        01000001    0x41       65       PCA9536
+
+  Note: The 'D' suffix of the Part Number denotes the SOIC-8 package
+
+  BUG REPORTS
+  -----------
+  Please report any bugs/issues/suggestions in the Github Repository of this library at: https://github.com/nadavmatalon/PCA9536/issues
+
+  LICENSE
+  -------
+
+  The MIT License (MIT)
+  Copyright (c) 2016 Nadav Matalon
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+  documentation files (the "Software"), to deal in the Software without restriction, including without
+  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+  conditions:
+  
+  The above copyright notice and this permission notice shall be included in all copies or substantial
+  portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+  LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "PCA9536.h"
+#include "utility/PCA9536InfoStr.h"
+
+PCA9536 pca9536;
+
+void setup() {
+    Serial.begin(9600);
+    Wire.begin();
+    while(!Serial);
+    pca9536.reset();
+    Serial.print(F("\n\nPCA9536 DEVICE INFO STRING TESTING"));
+    Serial.print(F("\n----------------------------------\n"));
+    Serial.print(F("\nCURRENT SETTINGS:\n"));
+    Serial.print(PCA9536InfoStr(pca9536));
+    Serial.print(F("\nCHANGING TO NEW SETTINGS..."));
+    pca9536.setMode(IO0, IO_OUTPUT);
+    pca9536.setMode(IO2, IO_OUTPUT);
+    pca9536.setState(IO2, IO_LOW);
+    pca9536.setPolarity(IO1, IO_INVERTED);
+    Serial.print(F("DONE\n"));
+    Serial.print(PCA9536InfoStr(pca9536));
+    Serial.print(F("\nRESETTING DEVICE..."));
+    pca9536.reset();
+    Serial.print(F("DONE\n"));
+    Serial.print(PCA9536InfoStr(pca9536));
+    Serial.print(F("\n\n"));
+}
+
+void loop() {}
--- utility/PCA9536InfoStr.h
+++ utility/PCA9536InfoStr.h
@@ -45,22 +45,15 @@ __asm volatile ("nop");
 namespace Pca9536 {
 
     const int  INFO_BUFFER_SIZE = 60;
-    const byte NUM_OF_INFO_STR  = 15;
+    const byte NUM_OF_INFO_STR  =  8;
 
     const char infoStr0[]  PROGMEM = "\nPCA9536 DEVICE INFORMATION";
     const char infoStr1[]  PROGMEM = "\n--------------------------";
     const char infoStr2[]  PROGMEM = "\nI2C ADDRESS:\t %d (%#X)";
     const char infoStr3[]  PROGMEM = "\nI2C COM STATUS:\t %sCONNECTED";
-    const char infoStr4[]  PROGMEM = "\nCONFIG BYTE:\t B%d%d%d%d%d%d%d%d";
-    const char infoStr5[]  PROGMEM = "\nDEVICE MODE:\t %s";
-    const char infoStr6[]  PROGMEM = "\nALERT TYPE:\t %s";
-    const char infoStr7[]  PROGMEM = "\nALERT MODE:\t ACTIVE-%s";
-    const char infoStr8[]  PROGMEM = "\nFAULT-QUEUE:\t %d FAULT%s";
-    const char infoStr9[]  PROGMEM = "\nRESOLUTION:\t %d-BIT";
-    const char infoStr10[] PROGMEM = "\nCONVERSION MODE: %s";
-    const char infoStr11[] PROGMEM = "\nDEGREES UNIT:\t %s";
-    const char infoStr12[] PROGMEM = "\nHYSTERESIS:\t %d.%d%s";
-    const char infoStr13[] PROGMEM = "\nLIMIT:\t\t %d.%d%s\n";
+    const char infoStr4[]  PROGMEM = "\nPIN\tMODE\tSTATE\tPOLARITY";
+    const char infoStr5[]  PROGMEM = "\n---\t----\t-----\t--------";
+    const char infoStr6[]  PROGMEM = "\nIO%d\t%sPUT\t%s\t%sINVERTED";
     const char errStr[]    PROGMEM = "\nI2C ERROR:\t ";
 
     const char * const infoStrs[NUM_OF_INFO_STR] PROGMEM = {
@@ -71,13 +64,6 @@ namespace Pca9536 {
         infoStr4,
         infoStr5,
         infoStr6,
-        infoStr7,
-        infoStr8,
-        infoStr9,
-        infoStr10,
-        infoStr11,
-        infoStr12,
-        infoStr13,
         errStr
     };
 
@@ -88,15 +74,12 @@ namespace Pca9536 {
     PCA9536_PString PCA9536InfoStr(const PCA9536& devParams) {
         char * ptr;
         char strBuffer[338];
-        int  devAddr = devParams._devAddr;
-        PCA9536 pca9536(devAddr);
-        byte comErrCode = pca9536.ping();
+        int  devAddr = DEV_ADDR;
+        PCA9536 pca9536;
+        pca9536.getMode(IO0);
+        byte comErrCode = pca9536.getComResult();
         PCA9536_PString resultStr(strBuffer, sizeof(strBuffer));
         char devInfoBuffer[INFO_BUFFER_SIZE];
-        byte tempUnit = devParams._tempUnit;
-        pca9536.setTempUnit(tempUnit ? FAHRENHEIT : CELSIUS);
-        byte  config = devParams._singleConfig ? devParams._singleConfig : pca9536.getConfig();
-        byte  fqVal = (config & 0x18) >> 2;
         for (byte i=0; i<4; i++) {
             ptr = (char *) pgm_read_word(&infoStrs[i]);
             if (i < 2)   snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr);
@@ -105,23 +88,26 @@ namespace Pca9536 {
             resultStr += devInfoBuffer;
         }
         if (!comErrCode) {
+            pin_t pins[4] = { IO0, IO1, IO2, IO3 };
             for (byte i=4; i<(NUM_OF_INFO_STR - 1); i++) {
                 ptr = (char *) pgm_read_word(&infoStrs[i]);
-                if (i == 4)  snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, (config >> 7)&1, (config >> 6)&1, (config >> 5)&1,
-                                (config >> 4)&1, (config >> 3)&1, (config >> 2)&1, (config >> 1)&1, config&1);
-                if (i == 5)  snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, (config ? "STANDBY" : "ON"));
-                if (i == 6)  snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, ((config >> 1)&1 ? "INTERRUPT": "COMPARATOR"));
-                if (i == 7)  snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, ((config >> 2)&1 ? "HIGH": "LOW"));
-                if (i == 8)  snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, (fqVal ? fqVal : 1), (fqVal ? "S" : ""));
-                if (i == 9)  snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, (((config & 0x60) >> 5) + 9));
-                if (i == 10) snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, (config ? "SINGLE-SHOT" : "CONTINUOUS"));
-                if (i == 11) snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, (tempUnit ? "FAHRENHEIT" : "CELSIUS"));
-                if (i == 12) snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, (int)hyst, abs((int)(hyst * 10) % 10), (tempUnit ? "F" : "C"));
-                if (i == 13) snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, (int)limit, abs((int)(limit * 10) % 10), (tempUnit ? "F" : "C"));
-                resultStr += devInfoBuffer;
+                if (i < 6)  {
+                    snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr);
+                    resultStr += devInfoBuffer;
+                }
+                if (i == 6)  {
+                    for (byte j=0; j<4; j++) {
+                        byte mode = pca9536.getMode(pins[j]);
+                        byte state = pca9536.getState(pins[j]);
+                        byte polarity = pca9536.getPolarity(pins[j]);
+                        snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, ptr, j, (mode ? "IN" : "OUT"), (state ? "HIGH" : "LOW"), (polarity ? "" : "NON-"));
+                        resultStr += devInfoBuffer;
+                    }
+                    resultStr += "\n";
+                }
             }
         } else {
-            snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, (char *) pgm_read_word(&infoStrs[14]));
+            snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, (char *) pgm_read_word(&infoStrs[7]));
             resultStr += devInfoBuffer;
             snprintf_P(devInfoBuffer, INFO_BUFFER_SIZE, (char *) pgm_read_word(&comCodes[comErrCode]));
             resultStr += devInfoBuffer;
@@ -129,7 +115,6 @@ namespace Pca9536 {
         }
         return resultStr;
     }
-
 }
 
 using namespace Pca9536;
